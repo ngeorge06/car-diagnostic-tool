746f5efb5eed343ed87c31d3cb4752a2ebb39990
=Neil George <=>
2026-09-20 00:45:56 +0400

    test sketch, to see if MCP2515 is wired correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#include <mcp_can.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define CS_PIN 5   // GPIO5, chip select pin for the MCP2515 CAN controller
+
+MCP_CAN CAN(CS_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial) { }  // wait for serial monitor
+
+  Serial.println("Initializing MCP2515...");
+
+  if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
+    Serial.println("MCP2515 initialized OK");
+  } else {
+    Serial.println("MCP2515 init FAILED - check wiring");
+  }
+
+  CAN.setMode(MCP_NORMAL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
